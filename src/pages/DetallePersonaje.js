4d81eb8202a4289760525a859b83186d42d4b2be
unknown <jmvelasquez935@misena.edu.co>
2022-09-17 22:41:03 -0500

    Detalle de los personajes

--- src/pages/DetallePersonaje.js
+++ src/pages/DetallePersonaje.js
@@ -2,28 +2,41 @@ import React, {useEffect, useState} from 'react';
 import NavScrollExample from '../components/NavbarApp'
 import FooterApp from '../components/FooterApp.js'
 import { useParams } from 'react-router-dom';
-import { UsePerson } from '../Hooks/UsePerson';
 import { singlePerson } from '../Hooks/FunctionPerson';
 
 export const DetallePersonaje = () => {
-    const { getAllPerson, setPersonajes } = UsePerson();
-
-    const params = useParams();
-    const paramslista = [params];
-
-    const map = paramslista.map((list) => list.name);
-
-    var name = map[0];
-
-    useEffect(() =>{
-        singlePerson(params.name)
-    }, []);
+  
+  
+  const Datos = useParams();
+  const [data, setData] = useState([])
+  const personaje = singlePerson(Datos.name)
+  useEffect(() => { personaje.then(res => setData(res)) }, []);
 
   return (
     <>
         <NavScrollExample/>
-        <div>
-          <h3>Nombre: {name}</h3>
+        <div className='DetailContainer'>
+          <div className='Personal'>
+          <h3 className='title'>{data.name}</h3>
+          <br/>
+          <p>Año de nacimiento: {data.birth_year}</p>
+          <p>Altura: {data.height} cm</p>
+          <p>Peso: {data.mass} kl</p>
+          <p>Color de ojos: {data.eye_color}</p>
+          <p>Color de cabello: {data.hair_color}</p>
+          <p>Color de piel: {data.skin_color}</p>
+          <p>Genero: {data.gender}</p>
+          </div>
+          <div className='MV'>
+            <div className='Movies'>
+              <h4 className='title'>Filmes</h4>
+              <p>N/A</p>
+            </div>
+            <div className='Vehiculos'>
+              <h4 className='title'>Vehiculos</h4>
+              <p>N/A</p>
+            </div>
+        </div>
         </div>
         <FooterApp/>
     </>
--- src/pages/Personajes.js
+++ src/pages/Personajes.js
@@ -1,12 +1,12 @@
 import { useEffect } from "react";
 import { UsePerson } from "../Hooks/UsePerson";
-import { PersonajeApp } from "../components/PersonajesApp";
 import NavScrollExample from '../components/NavbarApp'
 import FooterApp from '../components/FooterApp.js'
+import { Button } from "react-bootstrap";
 
 
 const Personajes = () => {
-  const { getAllPerson, dataPerson, Contador, disminuir, aumentar } =
+  const { getAllPerson, dataPerson, setDataPerson, Contador, disminuir, aumentar } =
     UsePerson();
   useEffect(() => {
     getAllPerson(Contador);
@@ -18,19 +18,22 @@ const Personajes = () => {
         <h1 className="Tpersonajes">PERSONAJES</h1>
     </center>
       <div className='Gplanet'>
-        {dataPerson.map((personajes) => (
-        <PersonajeApp
-            key={personajes.name}
-            name={personajes.name}
-            hair={personajes.hair_color}
-            eyes={personajes.eye_color}
-            skin={personajes.skin_color}
-            gender={personajes.gender}
-            height={personajes.height}
-            weigth={personajes.mass}
-            birthday={personajes.birth_year}
-          />
-        ))}
+        {dataPerson !== null ?
+        (dataPerson.map(dataPerson => (
+        <div key={setDataPerson} className='Cpersonajes'>
+        <center><h4><strong className='fuerte'></strong>{dataPerson.name}</h4></center><br/>
+        <p><strong className='fuerte'>Altura: </strong>{dataPerson.height}</p>
+        <p><strong className='fuerte'>Masa: </strong>{dataPerson.mass}</p>
+        <p><strong className='fuerte'>Color de cabello: </strong>{dataPerson.hair_color}</p>
+        <p><strong className='fuerte'>Color de piel: </strong>{dataPerson.skin_color}</p>
+        <p><strong className='fuerte'>Color de ojos: </strong>{dataPerson.eye_color}</p>
+        <p><strong className='fuerte'>Fecha de nacimiento: </strong>{dataPerson.birth_year}</p>
+        <p><strong className='fuerte'>Genero: </strong>{dataPerson.gender}</p>
+        <Button key={dataPerson.name} variant="outline-light" >
+          <a className='detalleB' href={`/Personajes/${dataPerson.url.split("/")[5]}`}>Detalles</a>
+        </Button>
+    </div>
+        ))) : ("Fuera de servicio")}
       </div>
       <div className="Paginacion">
         <button className="botonPag" onClick={disminuir}>
